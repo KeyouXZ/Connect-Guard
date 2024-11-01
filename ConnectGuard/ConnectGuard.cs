f9abf62f8de08c30de7ab57b0a9b9bbfd70871c5
Keyou <keyouz@outlook.com>
2024-11-01 21:58:53 +0700

    Change IP to Player Name & add delay before kicking player

--- ConnectGuard/ConnectGuard.cs
+++ ConnectGuard/ConnectGuard.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using TShockAPI;
+﻿using TShockAPI;
 using Terraria;
 using TerrariaApi.Server;
 using TShockAPI.Hooks;
@@ -19,7 +18,7 @@ namespace ConnectGuard
 
         public static Config config = null!;
         private Dictionary<string, int> loginAttempts = new Dictionary<string, int>();
-        private Dictionary<string, System.Timers.Timer> lockoutTimers = new Dictionary<string, System.Timers.Timer>();
+        private Dictionary<string, (System.Timers.Timer timer, DateTime startTime)> lockoutTimers = new Dictionary<string, (System.Timers.Timer, DateTime)>();
         private int MAX_ATTEMPTS;
         private int LOCKOUT_TIME;
 
@@ -36,7 +35,7 @@ namespace ConnectGuard
             MAX_ATTEMPTS = config.maxAttempts;
             LOCKOUT_TIME = config.lookoutTime * 60 * 1000;
 
-            TShock.Log.ConsoleInfo("[Connect Guard] Successfully reloaded configuration!");
+            TShock.Log.ConsoleInfo("[Connect Guard] Configuration reloaded!");
         }
 
         protected override void Dispose(bool disposing)
@@ -48,46 +47,68 @@ namespace ConnectGuard
           	base.Dispose(disposing);
         }
 
-        private void OnPlayerLogin(GreetPlayerEventArgs args)
+        private async void OnPlayerLogin(GreetPlayerEventArgs args)
         {
-            var playerIP = TShock.Players[args.Who].IP;
-            
-            if (lockoutTimers.ContainsKey(playerIP))
+            var playerName = TShock.Players[args.Who].Name;
+            if (args.Who < 0 || args.Who >= TShock.Players.Length || TShock.Players[args.Who] == null)
             {
-                TShock.Players[args.Who].Disconnect("This IP is temporarily locked due to too many failed login attempts.");
+                TShock.Log.ConsoleInfo("[Connect Guard] Invalid player index.");
                 return;
             }
 
-            if (loginAttempts.ContainsKey(playerIP))
-                loginAttempts[playerIP]++;
-            else
-                loginAttempts[playerIP] = 1;
-
-            if (loginAttempts[playerIP] > MAX_ATTEMPTS)
+            if (lockoutTimers.ContainsKey(playerName))
             {
-                TShock.Players[args.Who].Disconnect("Too many failed login attempts. This IP will be temporarily locked.");
-                LockoutIP(playerIP);
+
+                TShock.Log.ConsoleInfo("[Connect Guard] {0} trying to login. Kicking...", playerName);
+                var (timer, startTime) = lockoutTimers[playerName];
+                var remainingTime = (int)(timer.Interval - (DateTime.Now - startTime).TotalMilliseconds) / 1000;
+
+                await Task.Delay(1000);
+                TShock.Players[args.Who].Kick($"This player is temporarily locked due to too many failed login attempts. Try again in {remainingTime} seconds.", true, true);
+                return;
+            }
+
+
+            if (loginAttempts.ContainsKey(playerName))
+                loginAttempts[playerName]++;
+            else
+                loginAttempts[playerName] = 1;
+
+            if (loginAttempts[playerName] > MAX_ATTEMPTS)
+            {
+                TShock.Log.ConsoleInfo("[Connect Guard] {0} is temporarily locked!", playerName);
+                await Task.Delay(1000);
+                TShock.Players[args.Who].Kick("Too many failed login attempts. This player will be temporarily locked.", true, true);
+                LockoutIP(playerName);
+
             }
         }
 
-        private void LockoutIP(string playerIP)
+
+        private void LockoutIP(string playerName)
         {
             var timer = new System.Timers.Timer(LOCKOUT_TIME);
-            timer.Elapsed += (sender, e) => UnlockIP(playerIP);
+            var startTime = DateTime.Now;
+            timer.Elapsed += (sender, e) => UnlockIP(playerName);
             timer.AutoReset = false;
             timer.Start();
-            
-            lockoutTimers[playerIP] = timer;
-            loginAttempts.Remove(playerIP);
+
+            lockoutTimers[playerName] = (timer, startTime);
+            loginAttempts.Remove(playerName);
         }
 
-        private void UnlockIP(string playerIP)
+
+        private void UnlockIP(string playerName)
         {
-            if (lockoutTimers.ContainsKey(playerIP))
+            if (lockoutTimers.ContainsKey(playerName))
             {
-                lockoutTimers[playerIP].Stop();
-                lockoutTimers.Remove(playerIP);
+                lockoutTimers[playerName].timer.Stop();
+                lockoutTimers[playerName].timer.Dispose(); 
+                lockoutTimers.Remove(playerName);
+                loginAttempts.Remove(playerName);
+                TShock.Log.ConsoleInfo($"[Connect Guard] {playerName} has been unlocked.");
             }
         }
+
     }
 }
